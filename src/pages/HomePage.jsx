d38c3061feac8ba6066b22ede7c5dc2632ace13e
1id0r <lidornissim44@gmail.com>
2024-12-12 18:15:52 +0200

    fix bug

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
 import { Contact } from '../cmps/Contact'
 import { About } from '../cmps/About'
 import { Hero } from '../cmps/Hero'
-import { Portfolio } from '../cmps/portfolio'
+
+import { Portfolio } from '../cmps/Portfolio'
 
 export function HomePage() {
   return (
