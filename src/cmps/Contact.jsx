f9a061d48bf45bc8350322dacfb5c469910ecd99
1id0r <lidornissim44@gmail.com>
2024-12-12 18:09:18 +0200

    finishes

--- src/cmps/Contact.jsx
+++ src/cmps/Contact.jsx
@@ -58,7 +58,12 @@ export function Contact() {
     }
 
     try {
-      await emailjs.send('service_s3qhymc', 'template_3dp13tg', templateParams, 'P2Affzye293NeONIo')
+      await emailjs.send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        templateParams,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
 
       setStatus('success')
       setFormData({ name: '', email: '', subject: '', message: '' })
